449df83405085dda535837ebca2fd70a929c95ef
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-06-12 20:47:24 +0200

    build: initialize project using Spring Initializer

---
+++ src/test/java/com/equipo2/bytestournament/BytestournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo2.bytestournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytestournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
