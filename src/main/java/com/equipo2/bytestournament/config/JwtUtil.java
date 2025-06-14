9803f4684dbcb5bb7ddfec5d548c74c8023732eb
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-06-14 12:07:48 +0200

    docs: added complete documentation for Spring Security and improved logs

--- src/main/java/com/equipo2/bytestournament/config/JwtUtil.java
+++ src/main/java/com/equipo2/bytestournament/config/JwtUtil.java
@@ -7,40 +7,71 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+/**
+ * @Component Esta clase es un componente de Spring que proporciona utilidades para manejar JSON Web Tokens (JWT).
+ * @Value Anotaciones que permiten inyectar valores de propiedades desde el archivo de configuración.
+ * 
+ * JwtUtil es una clase que proporciona métodos para generar, validar y extraer información de tokens JWT.
+ * Utiliza la biblioteca JJWT para manejar la creación y validación de tokens.
+ */
 @Component
 public class JwtUtil {
     @Value("${jwt.secret}") private String secret;
     @Value("${jwt.expiration}") private long expiration;
 
+    /**
+     * generateToken genera un token JWT para un usuario autenticado.
+     * Este token incluye el nombre de usuario, la fecha de emisión y la fecha de expiración.
+     * 
+     * @param auth Authentication objeto que contiene la información del usuario autenticado.
+     * @return String token JWT generado.
+     */
     public String generateToken(Authentication auth) {
         String username = auth.getName();
         Date now = new Date();
         Date exp = new Date(now.getTime() + expiration * expiration);
 
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(now)
-                .setExpiration(exp)
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
+                .setSubject(username) // Establece el sujeto del token como el nombre de usuario
+                .setIssuedAt(now) // Establece la fecha de emisión del token.
+                .setExpiration(exp) // Establece la fecha de expiración del token.
+                .signWith(SignatureAlgorithm.HS512, secret) // Firma el token con el algoritmo HS512 y la clave secreta.
+                .compact(); // Genera una representacion en texto del token separado en tres bloques codificados en Base64.
     }
 
+    /**
+     * validateToken valida un token JWT.
+     * Este método intenta analizar el token utilizando la clave secreta.
+     * Si el token es válido, devuelve true; de lo contrario, devuelve false.
+     * 
+     * @param token String token JWT a validar.
+     * @return boolean true si el token es válido, false en caso contrario.
+     */
     public boolean validateToken(String token) {
        try {
+            // Intenta analizar el token con la clave secreta con la clave y analiza JWT.
             Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token);
-            
+            .setSigningKey(secret)
+            .parseClaimsJws(token); 
             return true;
        } catch (Exception e) {
             return false;
        }
     }
 
+    /**
+     * extractUsername extrae el nombre de usuario del token JWT.
+     * Este método analiza el token y obtiene el sujeto (nombre de usuario) del mismo.
+     * 
+     * @param token String token JWT del cual se extraerá el nombre de usuario.
+     * @return String nombre de usuario extraído del token.
+     */
     public String extractUsername(String token) {
+        // Intenta analizar el token con la clave secreta con la clave y analiza JWT.
         return Jwts.parser()
                 .setSigningKey(secret)
                 .parseClaimsJws(token)
+                // Si el token es válido, obtiene el cuerpo del token y extrae el sujeto (nombre de usuario o que es el subject en generateToken).
                 .getBody()
                 .getSubject();
     }
--- src/main/java/com/equipo2/bytestournament/service/CustomUserDetailsService.java
+++ src/main/java/com/equipo2/bytestournament/service/CustomUserDetailsService.java
@@ -4,32 +4,49 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-// import java.util.Optional;
-// import org.springframework.security.core.userdetails.User;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 
+/**
+ * CustomUserDetailsService es una implementación de UserDetailsService que se utiliza para cargar los detalles del usuario desde la base de datos.
+ * Esta clase es responsable de buscar un usuario por su nombre de usuario (email) y construir un objeto UserDetails que Spring Security utilizará para la autenticación y autorización.
+ * @Service Anotación que indica que esta clase es un servicio de Spring.
+ */
 @Service
 // TODO: Necesario implementar  UserRepository
 public class CustomUserDetailsService implements UserDetailsService {
-    // private final UserRepository userRepository;
+    /**
+     * userRepository es una instancia del repositorio de usuarios que se utiliza para acceder a los datos del usuario en la base de datos.
+     */
+    private final UserRepository userRepository;
 
-    // public CustomUserDetailsService(UserRepository userRepository) {
-    //     this.userRepository = userRepository;
-    // }
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-    // TODO: Implementar el método loadUserByUsername una vez que se tenga el UserRepository y el modelo de usuario
+    // TODO: Necesario implementar UserRepository y el modelo de usuario
+    /**
+     * loadUserByUsername es un método que busca un usuario por su nombre de usuario (email) y devuelve un objeto UserDetails, se ejecuta automaticamente por Spring Security durante el proceso de autenticación en el filtros JwtAuthentication Filter.
+     * @param username El nombre de usuario (email) del usuario a buscar.
+     * @return Un objeto UserDetails que contiene la información del usuario, incluyendo su nombre de usuario, contraseña y roles.
+     * @throws UsernameNotFoundException Si no se encuentra un usuario con el nombre de usuario proporcionado.
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Optional<User> user = userRepository.findByEmail(username);
+        Optional<User> user = userRepository.findByEmail(username);
 
-        // if(!user.isPresent())
-        //     throw new UsernameNotFoundException("Usuario no encontrado: " + username);
+        if(!user.isPresent())
+            throw new UsernameNotFoundException("Usuario no encontrado: " + username);
         
-        // return User.builder()
-        //         .username(user.getEmail())
-        //         .password(user.getPassword())
-        //         .roles(user.getRoles())
-        //         .build();
-        return null; // Solución temporal
+        return User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList()))
+                .build();
     }
     
 }
