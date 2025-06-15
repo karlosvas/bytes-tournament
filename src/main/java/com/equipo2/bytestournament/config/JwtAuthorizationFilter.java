448763e48d1303db6bb35677b7d95f5c1d0c0d44
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-06-15 23:11:25 +0200

    feat: add Docker, JavaDoc documentation and .jar

--- src/main/java/com/equipo2/bytestournament/config/JwtAuthorizationFilter.java
+++ src/main/java/com/equipo2/bytestournament/config/JwtAuthorizationFilter.java
@@ -1,6 +1,9 @@
 package com.equipo2.bytestournament.config;
 
 import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,6 +29,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
      */
     private final UserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
+    private final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    
 
     public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
--- src/main/java/com/equipo2/bytestournament/config/SecurityConfig.java
+++ src/main/java/com/equipo2/bytestournament/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -52,11 +52,7 @@ public class SecurityConfig {
      * @throws Exception si ocurre un error durante la configuración de seguridad.
      */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtUtil jwtUtil, CustomUserDetailsService uds) throws Exception {
-        // Especificar que el filtro solo aplica a login
-        JwtAuthenticationFilter jwtAuthFilter = new JwtAuthenticationFilter(jwtUtil, authenticationManager(http, uds));
-        jwtAuthFilter.setFilterProcessesUrl("/auth/login");
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtUtil jwtUtil, CustomUserDetailsService uds, AuthenticationManager authenticationManager) throws Exception {
         // Desabilita CSRF, establece la política de sesión como sin estado (stateless) y configura las reglas de autorización
         http
         .csrf(csrf -> csrf.disable())
@@ -68,7 +64,7 @@ public class SecurityConfig {
         )
         // Se llama a el filtro de autenticación JWT y posteriormente al filtro de autorización JWT
         .addFilter(
-            new JwtAuthenticationFilter(jwtUtil, authenticationManager(http, uds)))
+            new JwtAuthenticationFilter(jwtUtil, authenticationManager))
         .addFilterBefore(
             new JwtAuthorizationFilter(jwtUtil, uds), 
             UsernamePasswordAuthenticationFilter.class);
@@ -105,18 +101,16 @@ public class SecurityConfig {
     }
     
     /**
-     * authenticationManager define el administrador de autenticación que utiliza el proveedor de autenticación, e integra el provider en la cadena de la configuración de seguridad.
-     * Este método es necesario para que Spring Security pueda manejar la autenticación de usuarios.
+     * Expone el AuthenticationManager como un bean para ser utilizado en componentes que requieren autenticación.
+     * Spring Security automáticamente registrará en este AuthenticationManager cualquier AuthenticationProvider
+     * definido como bean en el contexto de la aplicación.
      * 
-     * @param http HttpSecurity para configurar la seguridad web.
-     * @param uds CustomUserDetailsService para cargar detalles del usuario.
-     * @return AuthenticationManager configurado con el proveedor de autenticación.
-     * @throws Exception si ocurre un error durante la configuración de autenticación.
+     * @param authConfig Configuración de autenticación proporcionada por Spring Security.
+     * @return El AuthenticationManager configurado por Spring Security.
+     * @throws Exception si ocurre un error durante la obtención del AuthenticationManager.
      */
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, CustomUserDetailsService uds) throws Exception {
-       return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .authenticationProvider(authProvider(uds))
-                .build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
