0395403ff98ede66f360dd3de8301e33caab0eb2
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-06-26 19:34:16 +0200

    fix: using MatchService and conflicts

--- src/main/java/com/equipo2/bytestournament/contoller/MessageController.java
+++ src/main/java/com/equipo2/bytestournament/contoller/MessageController.java
@@ -1,76 +1,49 @@
 package com.equipo2.bytestournament.contoller;
 
-import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.equipo2.bytestournament.DTO.MessageDTO;
-import com.equipo2.bytestournament.mapper.MessageMapper;
-import com.equipo2.bytestournament.model.Message;
-import com.equipo2.bytestournament.repository.MessageRepository;
-import com.equipo2.bytestournament.repository.UserRepository;
+import com.equipo2.bytestournament.service.MessageService;
+
 import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Message", description = "Controlador para la gestión de mensajes entre usuarios")
 public class MessageController {
- private final MessageRepository messageRepository;
-    private final UserRepository userRepository ;
-    private final MessageMapper messageMapper;
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     @GetMapping("/tournaments/{tournamentId}/messages")
     @PreAuthorize("hasRole('PLAYER') or hasRole('ADMIN')")
-    public ResponseEntity<List<MessageDTO>> getTournamentMessages(@PathVariable Long tournamentId) {
-        List<Message> messages = messageRepository.findByTournamentIdOrderByTimestampAsc(tournamentId);
-        return ResponseEntity.ok(messageMapper.messageToMessageDtos(messages));
+    public List<MessageDTO> getTournamentMessages(@PathVariable Long tournamentId) {
+        return messageService.getTournamentMessages(tournamentId);
     }
 
-     @PostMapping("/tournaments/{tournamentId}/messages")
+    @PostMapping("/tournaments/{tournamentId}/messages")
     @PreAuthorize("hasRole('PLAYER') or hasRole('ADMIN')")
-    public ResponseEntity<Void> sendTournamentMessage(@PathVariable Long tournamentId, @RequestBody MessageDTO dto) {
-        if (dto.getSenderId() == null || dto.getContent() == null) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        Message message = messageMapper.messageDtoToMessage(dto);
-        message.setTournamentId(tournamentId);
-        message.setMatchId(null);
-        message.setTimestamp(LocalDateTime.now());
-
-        messageRepository.save(message);
-        return ResponseEntity.ok().build();
+    public void sendTournamentMessage(@PathVariable Long tournamentId, @RequestBody MessageDTO dto) {
+        messageService.sendTournamentMessage(tournamentId, dto);
     }
 
     //Obtengo el mensaje de la partida
-    
     @GetMapping("/matches/{matchId}/messages")
     @PreAuthorize("hasRole('PLAYER') or hasRole('ADMIN')")
-    public ResponseEntity<List<MessageDTO>> getMatchMessages(@PathVariable Long matchId) {
-        List<Message> messages = messageRepository.findByMatchIdOrderByTimestampAsc(matchId);
-        return ResponseEntity.ok(messageMapper.messageToMessageDtos(messages));
+    public List<MessageDTO> getMatchMessages(@PathVariable Long matchId) {
+        return getMatchMessages(matchId);
     }
 
     //Enviar mensaje a partida
-
-@PostMapping("/matches/{matchId}/messages")
+    @PostMapping("/matches/{matchId}/messages")
     @PreAuthorize("hasRole('PLAYER') or hasRole('ADMIN')")
     public ResponseEntity<Void> sendMatchMessage(@PathVariable Long matchId, @RequestBody MessageDTO dto) {
-        if (dto.getSenderId() == null || dto.getContent() == null) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        Message message = messageMapper.messageDtoToMessage(dto);
-        message.setMatchId(matchId);
-        message.setTournamentId(null);
-        message.setTimestamp(LocalDateTime.now());
-
-        messageRepository.save(message);
-        return ResponseEntity.ok().build();
+        return messageService.sendMatchMessage(matchId, dto);
     }
 }
--- src/main/java/com/equipo2/bytestournament/model/Match.java
+++ src/main/java/com/equipo2/bytestournament/model/Match.java
@@ -1,185 +1,51 @@
 package com.equipo2.bytestournament.model;
 
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import com.equipo2.bytestournament.enums.Result;
-import com.equipo2.bytestournament.model.User;
-
-/**
- * Entidad JPA que representa una partida de un torneo de la aplicación de torneos.
- * <p>
- * Contiene el id del torneo, el jugador 1, el jugador 2 y la ronda actual.
- *
- * @author Christian Escalas
- */
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Match {
 
     /**
-     * Identificador único de la partida. No debe ser nulo
+     * id: Identificador único de la partida que se genera automáticamente.
+     * tournamentId: Identificador único del torneo al que pertenece la partida. No debe ser nulo.
+     * player1: Jugador 1 que participa en la partida. No debe ser nulo.
+     * player2: Jugador 2 que participa en la partida. No debe ser nulo.
+     * result: Resultado de la partida. No debe ser nulo ni vacío.
+     * round: Ronda actual de la partida. No debe ser nulo.
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
-    /**
-     * Identificador único del torneo. No debe ser nulo
-     */
-    // TODO: Relación entre Match y Tournament
     @Column(name = "tournament_id", updatable = false, nullable = false)
     private Long tournamentId;
 
-    /**
-     * Jugador 1 que participa en la partida. No debe ser nulo
-     */
     @Column(name = "player1", updatable = true, nullable = false)
     private User player1;
 
-    /**
-     * Jugador 2 que participa en la partida. No debe ser nulo
-     */
     @Column(name = "player2", updatable = true, nullable = false)
     private User player2;
 
-    /**
-     * Resultado de la partida. No debe ser nulo ni vacío.
-     */
     @Enumerated(EnumType.STRING)
     @Column(name = "result", updatable = true, nullable = false)
     private Result result;
 
-    /**
-     * Ronda actual de la partida
-     */
     @Column(name = "round", updatable = true, nullable = false)
     private Integer round;
-
-    public Match() {
-    }
-
-    /**
-     * Construye un nuevo torneo con nombre, número máximo de jugadores y estado.
-     *
-     * @param tournamentId id del torneo al que pertenece la partida.
-     * @param player1      Jugador1 que participa en la partida. No puede ser null ni vacío.
-     * @param player2      Jugador2 que participa en la partida. No puede ser null ni vacío.
-     * @param result       resultado actual de la partida. No puede ser null ni vacío.
-     * @param round        ronda actual de la partida.
-     */
-    public Match(Long tournamentId, User player1, User player2, Result result) {
-
-        if (tournamentId == null) {
-            throw new IllegalArgumentException("El torneo no puede ser nulo.");
-        }
-        if (player1 == null || player2 == null) {
-            throw new IllegalArgumentException("Ninguno de los jugadores pueden ser nulos.");
-        }
-        if (player1.getId() == null || player2.getId() == null) {
-            throw new IllegalArgumentException("Los jugadores deben tener id asignado");
-        }
-        if (player1.getId() == player2.getId()) {
-            throw new IllegalArgumentException("Un jugador no puede competir contra sí mismo, escoge otro jugador.");
-        }
-        if (result == null) {
-            throw new IllegalArgumentException("El resultado no puede ser null");
-        }
-
-        this.tournamentId = tournamentId;
-        this.player1 = player1;
-        this.player2 = player2;
-        this.result = result;
-
-        this.round = 1;
-
-
-        if (round < 1) {
-            throw new IllegalArgumentException("La ronda debe ser al menos la 1.")
-        } else {
-            this.round = round
-        }
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getTournamentId() {
-        return tournamentId;
-    }
-
-    public User getPlayer1() {
-        return player1;
-    }
-
-    public void setPlayer1(User player1) {
-        if (player1 == null) {
-            throw new IllegalArgumentException("El jugador no puede ser nulo.");
-        }
-        if (player1.getId() == null) {
-            throw new IllegalArgumentException("El jugador debe tener id asignado.");
-        }
-        if (player1.getId() == player2.getId()) {
-            throw new IllegalArgumentException("Un jugador no puede competir contra sí mismo, escoge otro jugador.");
-        }
-
-        this.player1 = player1;
-    }
-
-    public User getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer2(User player2) {
-
-        if (player2 == null) {
-            throw new IllegalArgumentException("El jugador no puede ser nulo.");
-
-
-        if (player1.getId() == player2.getId()) {
-            throw new IllegalArgumentException("Un jugador no puede competir contra sí mismo, escoge otro jugador.")
-        } else {
-            this.player2 = player2;
-       }
-        if (player2.getId() == null) {
-            throw new IllegalArgumentException("El jugador debe tener id asignado.");
-        }
-        if (player1.getId().equals(player2.getId())) {
-            throw new IllegalArgumentException("Un jugador no puede competir contra sí mismo, escoge otro jugador.");
-        }
-
-        this.player2 = player2;
-    }
-    }
-
-    public Result getResult() {
-        return result;
-    }
-
-    public void setResult(Result result) {
-        if (result == null) {
-            throw new IllegalArgumentException("El resultado no puede ser null");
-        }
-
-        this.result = result;
-    }
-
-    public Integer getRound() {
-        return round;
-    }
-
-    public void setRound(Integer round) {
-
-        if (round == null || round < this.round) {
-            throw new IllegalArgumentException("La ronda debe ser igual o superior a la que ya está la partida.");
-
-
-        if (round < 1) {
-            throw new IllegalArgumentException("La ronda debe ser al menos la 1.")
-        } else {
-            this.round = round;
-
-        }
-        this.round = round;
-    }
 }
